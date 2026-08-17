1b8ed3a5659c7827a2a5024916a5f449a75ff81f
xenomech <gokulsmenon227@gmail.com>
2026-08-18 01:01:34 +0530

    feat: add membership repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,20 +1,26 @@
 package repository
 
 import (
+	membershiprepo "ava/internal/repository/membership"
 	sessionrepo "ava/internal/repository/session"
+	tenantrepo "ava/internal/repository/tenant"
 	userrepo "ava/internal/repository/user"
 
 	"gorm.io/gorm"
 )
 
 type Repository struct {
-	User    userrepo.Repository
-	Session sessionrepo.Repository
+	User       userrepo.Repository
+	Tenant     tenantrepo.Repository
+	Membership membershiprepo.Repository
+	Session    sessionrepo.Repository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		User:    userrepo.NewRepository(db),
-		Session: sessionrepo.NewRepository(db),
+		User:       userrepo.NewRepository(db),
+		Tenant:     tenantrepo.NewRepository(db),
+		Membership: membershiprepo.NewRepository(db),
+		Session:    sessionrepo.NewRepository(db),
 	}
 }
