5bfb07f5ec83944ef4d80b65299fb1023e27b9af
xenomech <gokulsmenon227@gmail.com>
2026-08-18 17:32:11 +0530

    feat: add web env package

--- frontend/packages/env/src/web.ts
+++ frontend/packages/env/src/web.ts
@@ -1,1 +1,11 @@
-export {};
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  clientPrefix: "VITE_",
+  client: {
+    VITE_API_URL: z.url().default("http://localhost:8000/api/v1"),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
+});
